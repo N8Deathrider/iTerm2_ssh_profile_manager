85e0d2b31b3fc21d6a595541ee7601a61a53ad88
N8Deathrider <nath001c@gmail.com>
2023-08-24 08:19:12 -0600

    Changed the quotes in the setup.py to be consistent with my usual format

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='iTerm2_ssh_profile_manager',
-    version='',
-    packages=[''],
-    url='',
-    license='',
-    author='Sp8ns5982',
-    author_email='nath001c@gmail.com',
-    description=''
+    name="iTerm2_ssh_profile_manager",
+    version="",
+    packages=[""],
+    url="",
+    license="",
+    author="Sp8ns5982",
+    author_email="nath001c@gmail.com",
+    description=""
 )
